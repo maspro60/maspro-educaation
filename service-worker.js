78fb1d9b271bc148cb90161fc1d9bcfe414bc874
maspro60 <masproceo584@gmail.com>
2026-09-12 14:15:00 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "maspro-v1";
+const APP_FILES = ["./", "./index.html", "./manifest.json", "./icon.svg"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_FILES)));
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
